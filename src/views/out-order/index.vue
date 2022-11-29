4f0b3673760aeef046d2509fb4a16e5a04dd1103
chenyunc123 <1537299563@qq.com>
2022-11-29 15:11:05 +0800

    充电服务选择之后下一步无法点击
    充电服务选择之后下一步无法点击

--- src/views/out-order/index.vue
+++ src/views/out-order/index.vue
@@ -1,13 +1,13 @@
-<script setup lang='ts'>
-import { computed } from 'vue'
-import { getOutOrderListApi } from '@/api/out-order'
-import { getShopApi } from '@/api/shop'
-import { useStore } from '@/stores'
-import { localStorage } from '@/utils/local-storage'
+<script setup lang="ts">
+import { computed } from "vue";
+import { getOutOrderListApi } from "@/api/out-order";
+import { getShopApi } from "@/api/shop";
+import { useStore } from "@/stores";
+import { localStorage } from "@/utils/local-storage";
 
-const store = useStore()
-const route = useRoute()
-const router = useRouter()
+const store = useStore();
+const route = useRoute();
+const router = useRouter();
 
 const state = reactive({
   loading: true, // 下拉加载
@@ -24,36 +24,33 @@ const state = reactive({
   minusAmount: 0.0,
   allCheck: false, // 全部选择
   minPrice: 0.01, // 最小开票金额
-  accessToken: '',
-  orderType: '',
+  accessToken: "",
+  orderType: "",
   windowHeight: 0,
-})
+});
 
 /**
  * 计算总价
  */
 const totalPrice = computed({
   get() {
-    let totalPrice = 0
-    if (state.outOrderList.length === 0)
-      return totalPrice
+    let totalPrice = 0;
+    if (state.outOrderList.length === 0) return totalPrice;
 
     for (let i = 0; i < state.outOrderList.length; i++) {
-      const item = state.outOrderList[i]
-      if (item.status === true)
-        totalPrice += item.price
+      const item = state.outOrderList[i];
+      if (item.status === true) totalPrice += item.price;
     }
-    return (totalPrice - state.minusAmount).toFixed(2)
+    state.selectList = state.outOrderList.filter((x) => x.status === true);
+    return (totalPrice - state.minusAmount).toFixed(2);
   },
-  set() {
-  },
-})
+  set() {},
+});
 
 const checked = (index) => {
-  state.outOrderList[index].status = !state.outOrderList[index].status
-  state.selectList = state.outOrderList.filter(x => x.status === true)
-  state.allCheck = state.selectList.length === state.outOrderList.length
-}
+  state.outOrderList[index].status = !state.outOrderList[index].status;
+  state.allCheck = state.selectList.length === state.outOrderList.length;
+};
 
 /**
  * 获取全部负数（欠费）外部订单列表
@@ -64,18 +61,18 @@ const getMinusOutOrderList = () => {
     type: state.orderType,
     page: 0,
     size: 10000,
-  }
+  };
   getOutOrderListApi(params).then((res) => {
     if (res.code === 1) {
-      state.minusOutOrderList = res.content
+      state.minusOutOrderList = res.content;
       for (let i = 0; i < state.minusOutOrderList.length; i++)
-        state.minusAmount += Number(state.minusOutOrderList[i].price)
+        state.minusAmount += Number(state.minusOutOrderList[i].price);
     } else {
-      state.minusAmount = 0.0
-      state.minusOutOrderList = []
+      state.minusAmount = 0.0;
+      state.minusOutOrderList = [];
     }
-  })
-}
+  });
+};
 
 /**
  * 获取外部订单列表
@@ -85,78 +82,74 @@ const getOutOrderList = () => {
     type: state.orderType,
     page: state.pagination.page - 1,
     size: state.pagination.size,
-  }
+  };
   getOutOrderListApi(params).then((res) => {
-    state.loading = false
+    state.loading = false;
     if (res.code === 1) {
-      const data = res.content
-      state.pagination.totalPages = res.totalPages
-      for (const v of data)
-        v.status = false
+      const data = res.content;
+      state.pagination.totalPages = res.totalPages;
+      for (const v of data) v.status = false;
 
-      state.outOrderList = state.outOrderList.concat(data)
+      state.outOrderList = state.outOrderList.concat(data);
     } else {
-      state.empty = true
-      state.outOrderList = []
-      state.pagination.totalPages = 0
+      state.empty = true;
+      state.outOrderList = [];
+      state.pagination.totalPages = 0;
     }
-  })
-}
+  });
+};
 
 /**
  * 上拉加载
  */
 const loadMore = () => {
   if (state.pagination.page === state.pagination.totalPages) {
-    state.finished = true
-    return
+    state.finished = true;
+    return;
   }
-  state.pagination.page++
-  getOutOrderList()
-}
+  state.pagination.page++;
+  getOutOrderList();
+};
 
 /**
  * 全选
  */
 const change = () => {
   state.outOrderList.forEach((v) => {
-    return (v.status = state.allCheck)
-  })
-  if (state.allCheck === true)
-    state.selectList = state.outOrderList
-  else
-    state.selectList = []
-}
+    return (v.status = state.allCheck);
+  });
+  if (state.allCheck === true) state.selectList = state.outOrderList;
+  else state.selectList = [];
+};
 
 const goElectronicInvoice = () => {
-  localStorage.set('tot', totalPrice.value)
-  localStorage.set('seleted', JSON.stringify(state.selectList))
-  router.push('/make/merge-order')
-}
+  localStorage.set("tot", totalPrice.value);
+  localStorage.set("seleted", JSON.stringify(state.selectList));
+  router.push("/make/merge-order");
+};
 /**
  * 获取发票类型
  */
 const getShop = () => {
   getShopApi().then((res) => {
-    if (res.code === 1)
-      state.minPrice = res.content.minPrice
-  })
-}
+    if (res.code === 1) state.minPrice = res.content.minPrice;
+  });
+};
 
 const getWindowHeight = () => {
-  const clientHeight = document.documentElement.clientHeight
-  state.windowHeight = clientHeight - 87 - (store.ifShowH5NavBar ? 46 : 0)
-}
+  const clientHeight = document.documentElement.clientHeight;
+  state.windowHeight = clientHeight - 87 - (store.ifShowH5NavBar ? 46 : 0);
+};
 
 onMounted(() => {
-  state.accessToken = localStorage.get('accessToken')
-  state.orderType = route.query.type as string
-  localStorage.set('orderType', state.orderType)
-  getShop()
-  getMinusOutOrderList()
-  getOutOrderList()
-  getWindowHeight()
-})
+  state.accessToken = localStorage.get("accessToken");
+  state.orderType = route.query.type as string;
+  localStorage.set("orderType", state.orderType);
+  getShop();
+  getMinusOutOrderList();
+  getOutOrderList();
+  getWindowHeight();
+});
 </script>
 
 <template>
@@ -177,16 +170,30 @@ onMounted(() => {
   >
     <div v-if="state.minusOutOrderList.length > 0" style="margin-bottom: 30px">
       <p class="pd-left">
-        有{{ minusOutOrderList.length }}笔欠费金额，欠费金额小计：¥{{ minusAmount }}元
+        有{{ minusOutOrderList.length }}笔欠费金额，欠费金额小计：¥{{
+          minusAmount
+        }}元
       </p>
-      <div v-for="(item, index) in state.minusOutOrderList" :key="index" class="mint-checklist">
+      <div
+        v-for="(item, index) in state.minusOutOrderList"
+        :key="index"
+        class="mint-checklist"
+      >
         <div class="mint-cell-wrapper">
           <div>
-            <van-checkbox v-model="item.status" icon-size="15px" shape="square" />
+            <van-checkbox
+              v-model="item.status"
+              icon-size="15px"
+              shape="square"
+            />
           </div>
           <div class="order-right" @click="checked(index)">
             <van-cell-group :border="false">
-              <van-cell :title="`订单号：${item.no}`" :border="false" class="orderNum" />
+              <van-cell
+                :title="`订单号：${item.no}`"
+                :border="false"
+                class="orderNum"
+              />
               <van-cell
                 v-if="item.fields"
                 :title="Object.values(JSON.parse(item.fields))[0]"
@@ -204,14 +211,22 @@ onMounted(() => {
         </div>
       </div>
     </div>
-    <div v-for="(item, index) in state.outOrderList" :key="index" class="mint-checklist">
+    <div
+      v-for="(item, index) in state.outOrderList"
+      :key="index"
+      class="mint-checklist"
+    >
       <div class="mint-cell-wrapper">
         <div style="margin-top: 7px">
           <van-checkbox v-model="item.status" icon-size="15px" shape="square" />
         </div>
         <div class="order-right" @click="checked(index)">
           <van-cell-group :border="false">
-            <van-cell :title="`订单号：${item.no}`" :border="false" class="orderNum" />
+            <van-cell
+              :title="`订单号：${item.no}`"
+              :border="false"
+              class="orderNum"
+            />
             <van-cell
               v-if="item.fields"
               :title="Object.values(JSON.parse(item.fields))[0]"
@@ -241,14 +256,12 @@ onMounted(() => {
       <van-checkbox v-model="state.allCheck" @click="change()">
         本页全选
       </van-checkbox>
-      <template #tip>
-        最低开票金额{{ state.minPrice }}元
-      </template>
+      <template #tip> 最低开票金额{{ state.minPrice }}元 </template>
     </van-submit-bar>
   </div>
 </template>
 
-<style lang='less'>
+<style lang="less">
 .page-checklist {
   .price {
     .van-cell__value {
@@ -258,7 +271,7 @@ onMounted(() => {
 }
 </style>
 
-<style lang='less' scoped>
+<style lang="less" scoped>
 .page-checklist {
   margin-bottom: 87px;
   height: 100%;
