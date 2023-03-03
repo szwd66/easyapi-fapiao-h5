8fb694b29f02b1fe3b4e720c9beb67c61df6c45e
李华熠 <2736495579@qq.com>
2023-03-03 12:03:49 +0800

    增加京东快手页面

--- src/views/mall-order/jd.vue
+++ src/views/mall-order/jd.vue
@@ -1,24 +1,105 @@
 <script setup lang='ts'>
-const router = useRouter()
+import { showConfirmDialog, showDialog, showToast } from 'vant'
+import makeMixins from '../make/mixins/make'
 
-const gotoInvoiceList = () => {
-  router.replace('/invoice/record')
+const { checkEmailMobile } = makeMixins()
+
+const state = reactive({
+  shopName: '娃哈哈',
+  orderForm: {
+    number: '',
+    price: '',
+    email: '',
+  },
+  keyboardShow: false,
+})
+
+/**
+ * 点击开票
+ */
+const makeInvoice = () => {
+  if (state.orderForm.number === '') {
+    showToast('请输入订单号')
+    return
+  }
+
+  if (state.orderForm.price === '') {
+    showToast('请输入订单金额')
+    return
+  }
+
+  if (!checkEmailMobile(state.orderForm))
+    return
+  showConfirmDialog({
+    title: '提示',
+    message: '确认抬头和金额正确并申请开票吗？',
+  }).then(() => {
+  })
 }
-const gotoIndex = () => {
-  router.replace('/')
+
+/**
+ * 点击提示
+ */
+const openTips = () => {
+  showDialog({
+    title: '温馨提示',
+    message: '代码是写出来给人看的，附带能在机器上运行。',
+  }).then(() => {
+    // on close
+  })
 }
 </script>
 
 <template>
-  <div class="success">
-    <van-icon name="checked" color="#19be6b" size="120" />
-    <div class="success-button">
-      <van-button type="default" plain block round @click="gotoIndex">
-        返回首页
-      </van-button>
-      <van-divider hairline />
-      <van-button type="default" plain block round @click="gotoInvoiceList">
-        开票记录
+  <div class="mall-order">
+    <div class="image">
+      <img src="@/assets/images/jd.png">
+    </div>
+    <div class="title">
+      {{ state.shopName }}京东店铺——订单开票
+    </div>
+
+    <van-cell-group title="京东订单信息（已在京东申请的开票）" inset>
+      <van-field
+        v-model="state.orderForm.number"
+        label="订单号"
+        placeholder="请输入订单号"
+        required
+      />
+      <van-field
+        v-model="state.orderForm.price"
+        readonly
+        clickable
+        label="金额"
+        placeholder="请输入订单金额"
+        @touchstart.stop="state.keyboardShow = true"
+      />
+      <van-number-keyboard
+        v-model="state.orderForm.price"
+        :show="state.keyboardShow"
+        theme="custom"
+        extra-key="."
+        close-button-text="完成"
+        @blur="state.keyboardShow = false"
+      />
+    </van-cell-group>
+    <div class="tips-forget" @click="openTips">
+      我不知道订单在哪里
+    </div>
+    <van-cell-group title="接收方式" inset>
+      <van-field
+        v-model="state.orderForm.email"
+        label="邮箱"
+        placeholder="请输入接收邮箱"
+      />
+    </van-cell-group>
+    <div class="tips">
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+    </div>
+    <div class="bottom fixed-bottom-bgColor">
+      <van-button type="primary" class="submit" block @click="makeInvoice">
+        开票
       </van-button>
     </div>
   </div>
--- src/views/mall-order/kuaishou.vue
+++ src/views/mall-order/kuaishou.vue
@@ -1,24 +1,147 @@
 <script setup lang='ts'>
-const router = useRouter()
+import { showConfirmDialog, showDialog, showToast } from 'vant'
+import makeMixins from '../make/mixins/make'
+import { localStorage } from '@/utils/local-storage'
 
-const gotoInvoiceList = () => {
-  router.replace('/invoice/record')
+const { checkEmailMobile } = makeMixins()
+
+const state = reactive({
+  shopName: '娃哈哈',
+  orderForm: {
+    number: '',
+    price: '',
+    email: '',
+  },
+  keyboardShow: false,
+  isShow: false,
+  isHide: false,
+  ifPaper: localStorage.get('ifPaper'),
+  ifElectronic: localStorage.get('ifElectronic'),
+  company: {
+    companyId: '',
+  },
+  invoiceForm: {
+    outOrderNo: `H5${new Date().getTime()}`,
+    type: '企业',
+    category: '增值税电子普通发票',
+    property: localStorage.get('ifElectronic') ? '纸质' : '电子',
+    purchaserName: '',
+    purchaserTaxpayerNumber: '',
+    purchaserAddress: '',
+    purchaserPhone: '',
+    purchaserBank: '',
+    purchaserBankAccount: '',
+    addrMobile: '',
+    email: '',
+    remark: '',
+    price: 0.0,
+    companyId: '',
+    outOrderIds: '',
+  },
+})
+
+/**
+ * 点击开票
+ */
+const makeInvoice = () => {
+  if (state.orderForm.number === '') {
+    showToast('请输入订单号')
+    return
+  }
+
+  if (state.orderForm.price === '') {
+    showToast('请输入订单金额')
+    return
+  }
+
+  if (!checkEmailMobile(state.orderForm))
+    return
+  showConfirmDialog({
+    title: '提示',
+    message: '确认抬头和金额正确并申请开票吗？',
+  }).then(() => {
+  })
 }
-const gotoIndex = () => {
-  router.replace('/')
+
+/**
+ * 点击提示
+ */
+const openTips = () => {
+  showDialog({
+    title: '温馨提示',
+    message: '代码是写出来给人看的，附带能在机器上运行。',
+  }).then(() => {
+    // on close
+  })
+}
+
+const receiveCompany = (val) => {
+  state.company = val
 }
 </script>
 
 <template>
-  <div class="success">
-    <van-icon name="checked" color="#19be6b" size="120" />
-    <div class="success-button">
-      <van-button type="default" plain block round @click="gotoIndex">
-        返回首页
-      </van-button>
-      <van-divider hairline />
-      <van-button type="default" plain block round @click="gotoInvoiceList">
-        开票记录
+  <div class="mall-order">
+    <div class="image">
+      <img src="@/assets/images/kuaishou.png">
+    </div>
+    <div class="title">
+      {{ state.shopName }}快手店铺——订单开票
+    </div>
+    <Invoice
+      :is-show="state.isShow"
+      :is-hide="state.isHide"
+      :if-electronic="state.ifElectronic"
+      :invoice-form="state.invoiceForm"
+      :if-paper="state.ifPaper"
+      :company="state.company"
+      @getCompany="receiveCompany"
+      @getInvoiceCategory="receiveCategory"
+      @getInvoiceProperty="receiveProperty"
+    />
+
+    <van-cell-group title="订单信息" inset>
+      <van-field
+        v-model="state.orderForm.number"
+        label="订单号"
+        placeholder="请输入订单号"
+        required
+      />
+      <van-field
+        v-model="state.orderForm.price"
+        readonly
+        clickable
+        label="金额"
+        placeholder="请输入订单金额"
+        required
+        @touchstart.stop="state.keyboardShow = true"
+      />
+      <van-number-keyboard
+        v-model="state.orderForm.price"
+        :show="state.keyboardShow"
+        theme="custom"
+        extra-key="."
+        close-button-text="完成"
+        @blur="state.keyboardShow = false"
+      />
+    </van-cell-group>
+    <div class="tips-forget" @click="openTips">
+      我不知道订单在哪里
+    </div>
+    <van-cell-group title="接收方式" inset>
+      <van-field
+        v-model="state.orderForm.email"
+        label="邮箱"
+        placeholder="请输入接收邮箱"
+      />
+    </van-cell-group>
+    <div class="tips">
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+    </div>
+    <div class="bottom fixed-bottom-bgColor">
+      <van-button type="primary" class="submit" block @click="makeInvoice">
+        开票
       </van-button>
     </div>
   </div>
